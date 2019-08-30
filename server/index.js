f48eeece0618a0ee96265ce2acbf533c4fb3c263
LiuFei <LFGloaming@163.com>
2019-08-30 09:57:44 +0800

    增加模版引擎中间件

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
 const Koa = require('koa')
 const app = new Koa()
-const { htmlTpl,ejsTpl,pugTpl } = require('./tpl')
-const ejs = require('ejs')
-const pug = require('pug')
+const views = require('koa-views')
+const { resolve } = require('path')
+// const { htmlTpl,ejsTpl,pugTpl } = require('./tpl')
+// const ejs = require('ejs')
+// const pug = require('pug')
+
+app.use(views(resolve(__dirname,'views'),{
+  extension:'pug'
+}))
 
 app.use(async (ctx,next) => {
-  ctx.type = 'text/html;charset=utf-8'
-  ctx.body = pug.render(pugTpl,{
+  // ctx.type = 'text/html;charset=utf-8'
+  // ctx.body = pug.render(pugTpl,{
+  //   you:'Luke',
+  //   me:'Scott'
+  // })
+
+  await ctx.render('index',{
     you:'Luke',
     me:'Scott'
   })
